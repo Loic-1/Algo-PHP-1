6ccfb0c2f511f408036167e11e165776f34c93be
Loic-1 <loic.vasile130@gmail.com>
2024-07-15 15:51:40 +0200

    exo 5

---
+++ exo5.php
@@ -0,0 +1,9 @@
+<h2>Exercice 5</h2>
+<p>Ecrire un algorithme qui déclare une valeur en francs et qui la convertit en euros. 
+Attention, la valeur générée devra être arrondie à 2 décimales.</p>
+<h2>Résultat</h2>
+<?php
+
+$valeurFR = 100;
+$valeurEUR = round($valeurFR / 6.5596, 2);
+echo $valeurFR, " francs français valent aujourd'hui (en 2024) ", $valeurEUR, " euros.";
